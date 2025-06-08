2fe9d6782d96020294f89354c0847a3b9cf18408
Marilane24 <marilanekamga@gmail.com>
2025-06-08 02:53:58 +0100

    feature/#3-dto

---
+++ src/main/java/marilane/store/store/dto/EmployeDTO.java
@@ -0,0 +1,22 @@
+package marilane.store.store.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import marilane.store.store.model.TypePoste;
+
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record EmployeDTO(
+        UUID id,
+        String nom,
+        String email,
+        Integer anciennete,
+        Integer salaire,
+        TypePoste poste,
+        String departement
+) {
+}
+
